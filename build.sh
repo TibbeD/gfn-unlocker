78f62be6f006b491e008ac9adddd88af2f17bf57
TibbeD <95880006+TibbeD@users.noreply.github.com>
2021-12-19 09:17:16 +0100

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-OUT_PATH='build'
-OUT_PKG='gfn-unlocker.zip'
-INCLUDE='icons/* background.js manifest.json popup.html'
-
-rm -rf $OUT_PATH
-mkdir -p $OUT_PATH
-zip -1r "$OUT_PATH/$OUT_PKG" ./ -i $INCLUDE
